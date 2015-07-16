8c4a6f07165719b8e04e5f9061634b28165dee25
Robert Haritonov <r@rhr.me>
2015-07-16 15:22:24 +0200

    improve error handling

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-var path = require('path'),
-    cmd = require('./lib/utils/cmd'),
-    fs = require('fs'),
-    Q = require('q'),
-    osenv = require('osenv'),
-    extractUser = require('./lib/utils/extractUser'),
-    extractPassword = require('./lib/utils/extractPassword'),
-    parseString = require('xml2js').parseString;
+var path = require('path');
+var cmd = require('./lib/utils/cmd');
+var fs = require('fs');
+var Q = require('q');
+var osenv = require('osenv');
+var extractUser = require('./lib/utils/extractUser');
+var extractPassword = require('./lib/utils/extractPassword');
+var parseString = require('xml2js').parseString;
 
-var fetch = exports.fetch = function() {
+exports.fetch = function() {
   var deferred = Q.defer();
   var m2Path = path.join(osenv.home(), '.m2');
   var settingsXmlPath = path.join(m2Path, 'settings.xml');
   var settingsSecurityXmlPath = path.join(m2Path, 'settings-security.xml');
 
-  if (fs.existsSync(settingsSecurityXmlPath)) {
+  var settingsExists = fs.existsSync(settingsXmlPath);
+  var secureSettingsExists = fs.existsSync(settingsSecurityXmlPath);
+
+  if (settingsExists && secureSettingsExists) {
     cmd(__dirname + '/lib/settings-decoder/bin/settings-decoder', ['-f', settingsXmlPath, '-s', settingsSecurityXmlPath])
       .then(function (stdout){
           var username = extractUser(stdout[0]);
@@ -24,8 +27,13 @@ var fetch = exports.fetch = function() {
           });
       })
       .fail(deferred.reject);
-  } else {
+  } else if (settingsExists) {
     parseString(fs.readFileSync(settingsXmlPath), {explicitArray: false}, function (err, xml) {
+      if (err) {
+        deferred.reject(err);
+        return;
+      }
+
       if (xml.settings && xml.settings.servers && xml.settings.servers.server[0]) {
         var username = xml.settings.servers.server[0].username;
         var password = xml.settings.servers.server[0].password;
@@ -35,9 +43,11 @@ var fetch = exports.fetch = function() {
             password: password
         });
       } else {
-        deferred.reject('No credentials found');
+        deferred.reject('No credentials found.');
       }
     });
+  } else {
+    deferred.reject('No maven configuration found.')
   }
 
   return deferred.promise;
