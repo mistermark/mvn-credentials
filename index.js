d4447ca98c2ef6461b346db85889723b5417a10c
Mark de Jong <mark@markdejong.com>
2016-05-25 14:18:56 +0200

    Adding check for repoName on fetching creds

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var extractUser = require('./lib/utils/extractUser');
 var extractPassword = require('./lib/utils/extractPassword');
 var parseString = require('xml2js').parseString;
 
-exports.fetch = function() {
+exports.fetch = function(repoName) {
   var deferred = Q.defer();
   var m2Path = path.join(osenv.home(), '.m2');
   var settingsXmlPath = path.join(m2Path, 'settings.xml');
@@ -34,20 +34,47 @@ exports.fetch = function() {
         return;
       }
 
-      if (xml.settings && xml.settings.servers && xml.settings.servers.server[0]) {
-        var username = xml.settings.servers.server[0].username;
-        var password = xml.settings.servers.server[0].password;
+      var repo = {};
 
-        deferred.resolve({
-            username: username,
-            password: password
+      var getRepoId = function(cb) {
+        for (var i = 0; i < xml.settings.profiles.profile.repositories.repository.length; i++) {
+          if(xml.settings.profiles.profile.repositories.repository[i].url.indexOf(repoName) > -1) {
+            if(cb) cb(xml.settings.profiles.profile.repositories.repository[i].id);
+          }
+        }
+      };
+      var getRepoCredentials = function(cb){
+        for (var i = 0; i < xml.settings.servers.server.length; i++) {
+          // console.log(xml.settings.servers.server[i]);
+          if(xml.settings.servers.server[i].id === repo.id) {
+            if(cb) cb({
+              "username": xml.settings.servers.server[i].username,
+              "password": xml.settings.servers.server[i].password
+            });
+          }
+        }
+      };
+
+      if(xml.settings && xml.settings.profiles && xml.settings.profiles.profile.length > 0) {
+        getRepoId(function(res) {
+          repo.id = res;
         });
-      } else {
+      }
+      else if (xml.settings && xml.settings.servers && xml.settings.servers.server.length > 0) {
+        getRepoCredentials(function(res) {
+          deferred.resolve({
+            username: res.username,
+            password: res.password
+          });
+        });
+      }
+      else {
         deferred.reject('No credentials found.');
       }
+
     });
   } else {
-    deferred.reject('No maven configuration found.')
+    deferred.reject('No maven configuration found.');
   }
 
   return deferred.promise;
